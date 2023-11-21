3979586b53f9e24ddb244699b98ca907040563f8
EnthusedFear <enthusedfir@gmail.com>
2023-11-21 11:06:54 +0200

    refactoring config.go

--- config/config.go
+++ config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"errors"
-	"fmt"
 	"os"
 	"regexp"
 	"strconv"
@@ -12,38 +10,17 @@ import (
 	"github.com/4rchr4y/gdk/regex"
 )
 
-func requireEnvVar(key string) (string, error) {
-	value, ok := os.LookupEnv(key)
-	if !ok {
-		return "", errors.New("") // TODO: set err message
-	}
+func MustGetInt(key string) int                { return must.Must(strconv.Atoi(os.Getenv(key))) }
+func MustGetUint(key string) uint64            { return must.Must(strconv.ParseUint(os.Getenv(key), 10, 64)) }
+func MustGetFloat64(key string) float64        { return must.Must(strconv.ParseFloat(os.Getenv(key), 64)) }
+func MustGetString(key string) string          { return must.MustBeOk(os.LookupEnv(key)) }
+func MustGetDuration(key string) time.Duration { return must.Must(time.ParseDuration(os.Getenv(key))) }
+func MustGetUrl(key string) string {
+	value := must.MustBeOk[string](os.LookupEnv(key))
 
-	return value, nil
-}
+	matched, _ := regexp.MatchString(regex.UrlPatternString, value)
 
-func MustGetInt(key string) int         { return must.Must(strconv.Atoi(os.Getenv(key))) }
-func MustGetUint(key string) uint64     { return must.Must(strconv.ParseUint(os.Getenv(key), 10, 64)) }
-func MustGetFloat64(key string) float64 { return must.Must(strconv.ParseFloat(os.Getenv(key), 64)) }
-func MustGetString(key string) string   { return must.Must(requireEnvVar(key)) }
-
-// func MustGetString(key string) string {
-// 	value, ok := os.LookupEnv(key)
-// 	if !ok {
-// 		err := fmt.Errorf(""environment variable '%s' not found", key)
-// 		panic(err)
-// 	}
-
-// 	return value
-// }
-
-func GetStringWithDefault(key string, defaultValue string) string {
-	// TODO: use MustBeOk
-	value, ok := os.LookupEnv(key)
-	if !ok {
-		return defaultValue
-	}
-
-	return value
+	return must.MustBeOk[string](value, matched)
 }
 
 func GetIntWithDefault(key string, defaultValue int) int {
@@ -64,15 +41,6 @@ func GetUintWithDefault(key string, defaultValue uint) uint {
 	return uint(value)
 }
 
-func GetFloat32WithDefault(key string, defaultValue float32) float32 {
-	value, err := strconv.ParseFloat(os.Getenv(key), 32)
-	if err != nil {
-		return defaultValue
-	}
-
-	return float32(value)
-}
-
 func GetFloat64WithDefault(key string, defaultValue float64) float64 {
 	value, err := strconv.ParseFloat(os.Getenv(key), 64)
 	if err != nil {
@@ -82,15 +50,19 @@ func GetFloat64WithDefault(key string, defaultValue float64) float64 {
 	return value
 }
 
-func MustGetUrl(key string) string {
-	value := os.Getenv(key)
-	matched, err := regexp.MatchString(regex.UrlPatternString, value)
-	if !matched {
-		err := fmt.Errorf("environment variable '%s' by key '%s' is not matching URL pattern", value, key)
-		panic(err)
+func GetStringWithDefault(key string, defaultValue string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
 	}
 
-	must.Must(value, err)
+	return value
+}
+func GetDurationWithDefault(key string, defaultValue time.Duration) time.Duration {
+	value, err := time.ParseDuration(os.Getenv(key))
+	if err != nil {
+		return defaultValue
+	}
 
 	return value
 }
@@ -100,22 +72,6 @@ func GetUrlWithDefault(key string, defaultValue string) string {
 	value := os.Getenv(key)
 	matched, err := regexp.MatchString(regex.UrlPatternString, value)
 	if err != nil || !matched {
-		return defaultValue
-	}
-
-	return value
-}
-
-func MustGetDuration(key string) time.Duration {
-	value, err := time.ParseDuration(os.Getenv(key))
-	must.Must(value, err)
-
-	return value
-}
-
-func GetDurationWithDefault(key string, defaultValue time.Duration) time.Duration {
-	value, err := time.ParseDuration(os.Getenv(key))
-	if err != nil {
 		return defaultValue
 	}
 
