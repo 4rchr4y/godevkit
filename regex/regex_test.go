dbfcaecc139d174c452c2bc6cec187392e26f199
EnthusedFear <enthusedfir@gmail.com>
2023-11-21 11:07:39 +0200

    added regex package

---
+++ regex/regex_test.go
@@ -0,0 +1,47 @@
+package regex
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUrlPattern(t *testing.T) {
+	t.Run("valid: valid url", func(t *testing.T) {
+		url := "https://www.google.com/"
+
+		matched, err := regexp.MatchString(UrlPatternString, url)
+
+		assert.True(t, matched)
+		assert.NoError(t, err)
+	})
+
+	t.Run("valid: valid url", func(t *testing.T) {
+		url := "https://xkcd.com/2293/"
+
+		matched, err := regexp.MatchString(UrlPatternString, url)
+
+		assert.True(t, matched)
+		assert.NoError(t, err)
+	})
+
+	t.Run("valid: valid url", func(t *testing.T) {
+		url := "https://github.com/orgs/4rchr4y/repositories?type=source"
+
+		matched, err := regexp.MatchString(UrlPatternString, url)
+
+		assert.True(t, matched)
+		assert.NoError(t, err)
+	})
+
+	t.Run("invalid: empty string as a value", func(t *testing.T) {
+		url := ""
+
+		matched, err := regexp.MatchString(UrlPatternString, url)
+
+		assert.False(t, matched)
+		assert.NoError(t, err)
+	})
+
+}
